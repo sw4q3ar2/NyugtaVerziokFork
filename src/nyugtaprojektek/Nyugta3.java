7b1d9a1bad5f92efefb87ddd3c1f20c14af6cf39
sw4q3ar2 <biggercsgo@gmail.com>
2022-03-16 15:32:51 +0100

    igen

--- src/nyugtaprojektek/Nyugta3.java
+++ src/nyugtaprojektek/Nyugta3.java
@@ -1,23 +1,17 @@
 package nyugtaprojektek;
-/* Probléma:
-tüntessük el a kézzel beírt szóközöket,
-a huf ne tartalmazzon eurót
-a kedvezmény helyett legyen szervízdíj, amit hozzá kell adni a végössdzeghez
-*/
+
 public class Nyugta3 {
 
     public static void main(String[] args) {
         String csillagok = "********************";
         System.out.println(csillagok);
-        //System.out.println("     Nyugta 3");
+        
         System.out.printf("%14s\n", "Nyugta 3");
         System.out.println(csillagok);
         
         final String HUF = "Ft";
         int tetel1 = 350, tetel2 = 90, tetel3 = 1320;
-//        System.out.printf("Tétel 1:     %d %s\n", tetel1, huf);
-//        System.out.printf("Tétel 1:      %d %s\n", tetel2, huf);
-//        System.out.printf("Tétel 3:    %d %s\n", tetel3, huf);
+
         System.out.printf("%10s: %5d %s\n", "Tétel 1", tetel1, HUF);
         System.out.printf("%10s: %5d %s\n", "Tétel 2", tetel2, HUF);
         System.out.printf("%10s: %5d %s\n", "Tétel 3", tetel3, HUF);
@@ -26,7 +20,7 @@ public class Nyugta3 {
         System.out.println(duplaVonal);
         
         int osszesen = tetel1 + tetel2 + tetel3;
-        //System.out.printf("Összesen:   %d %s\n", osszesen, huf);
+        
         System.out.printf("%10s: %5d %s\n", "Összesen", osszesen, HUF);
         
         String szaggatottVonal = "--------------------";
@@ -34,15 +28,15 @@ public class Nyugta3 {
         
         int szervizDijMertek = 10;
         int szervizDij = osszesen / szervizDijMertek;
-        //System.out.printf("Kedvezmény:  %d %s\n", kedvezmeny, huf);
+        
         System.out.printf("%10s: %5d %s\n", "Szervízdíj", szervizDij, HUF);
         System.out.printf("(%d%%)\n", szervizDijMertek);
         
         System.out.println(duplaVonal);
         
-        //int fizetendo = osszesen - szervizDij;
+        
         int fizetendo = osszesen + szervizDij;
-        //System.out.printf("Fizetendő:  %d %s\n", fizetendo, huf);
+        
         System.out.printf("%10s:  %d %s\n", "Fizetendő", fizetendo, HUF);
         double euro = fizetendo / 350.0;
         //HUF = "\u20ac";//EZ NEM JÓ!!!!
